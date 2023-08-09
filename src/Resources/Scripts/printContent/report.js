d1db53e0f4578919d88d0c9f61e1c5f34dd2c455
Sabarinathan Arthanari <Sabarinathan.Arthanari@avenuinsights.com>
2023-08-10 00:15:30 +0530

    data table warning handling

--- src/Resources/Scripts/printContent/report.js
+++ src/Resources/Scripts/printContent/report.js
@@ -3,10 +3,16 @@ window.PagedConfig = {
     auto: false
     //, after: (flow) => { console.log("after", flow) },
 };
-
 // Call the loadAndReplace function when the page loads
 window.onload = function ()
 {
+    // https://stackoverflow.com/questions/18007630/how-to-disable-warning-datatables-warning-requested-unknown-parameter-from-the-d
+    $.fn.dataTable.ext.errMode = 'none'; //to suppress the warnings from data table library in case if a property is missing.
+    $('.content .dataTable').on('error.dt', function (e, settings, techNote, message)
+    {
+        console.error('An error has been reported by DataTables: ', message);
+    });
+    
     const contentSelector = '.content';
     const contentDOMElement = $(contentSelector);
     var tableIndex = 1;
